ea1c94125cf2f675079ebe3aa49cdff50303809c
Vandit Shah <vanditashah12@gmail.com>
2021-10-24 18:20:24 +0530

    second commit

--- config/key.js
+++ config/key.js
@@ -1,5 +1,4 @@
 module.exports = {
-  MongoURI:
-    "mongodb+srv://vandit-admin:1234@cluster0.tlenm.mongodb.net/Random?retryWrites=true&w=majority",
+  MongoURI: "MONGO-URI",
   JwtKey: "secret",
 };
